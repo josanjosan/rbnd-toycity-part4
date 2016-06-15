58f3cb5bde53d079e613a1b07b9d57823fe0cd63
José Robles <joseroblesoneto@gmail.com>
2016-06-15 00:20:53 -0400

    delete now unnecesary data_path variable from db_seed method

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,8 +4,7 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  data_path = "../data/data.csv"# Your code goes here!
-  100.times do
+  100.times do# Your code goes here!
       Product.create(brand: Faker::Commerce.department, name: Faker::Commerce.product_name, price: Faker::Commerce.price)
   end
 end
