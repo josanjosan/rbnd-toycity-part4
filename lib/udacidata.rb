09e51fba672a5e010b9ccc8075b7d8831068265c
José Robles <joseroblesoneto@gmail.com>
2016-06-19 11:25:25 -0400

    add find_by_name and find_by_brand class methods to Product class

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,6 +4,7 @@ require 'csv'
 
 class Udacidata
   @@data_path = "../data/data.csv"
+  @@attrs = [:brand, :name]
   
   def self.create(opts = {})
   	new_item = Product.new(brand: opts[:brand], name: opts[:name], price: opts[:price])
@@ -32,17 +33,31 @@ class Udacidata
   end
 
   def self.find(id)
-    found = all.select { |product| product.id == id}
+    found = all.select { |product| product.id == id }
     return found.first
   end
 
   def self.destroy(id)
     destroyed_product = self.find(id)
     data = CSV.table(@@data_path, headers: false)
-    data.delete_if { |row| row[0] == destroyed_product.id}
+    data.delete_if { |row| row[0] == destroyed_product.id }
     CSV.open(@@data_path, "wb") do |csv|
       data.each { |row| csv << row }
     end
     return destroyed_product
   end
+
+  def self.create_find_by_methods
+    @@attrs.each do |attr|
+      find_by_method = %Q{
+        def find_by_#{attr}(attr_value)
+         found = all.select { |product| product.#{attr} ==  attr_value}
+         return found.first
+        end
+        }
+      instance_eval(find_by_method)
+    end
+  end
+
+  create_find_by_methods
 end
