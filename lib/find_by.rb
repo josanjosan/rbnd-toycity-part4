44e4ca26cd73e8267c496018ac646e5f0e4a9e16
José Robles <joseroblesoneto@gmail.com>
2016-06-20 23:05:05 -0400

    move find_by_#{attribute} methods to find_by.rb

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,13 @@
 class Module
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
-  end
+    attributes.each do |attr|
+      find_by_method = %Q{
+        def find_by_#{attr}(attr_value)
+          found = all.select { |product| product.#{attr} ==  attr_value}
+          return found.first
+        end
+        }
+      instance_eval(find_by_method)
+    end# Your code goes here!
+  end# Hint: Remember attr_reader and class_eval
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -47,18 +47,6 @@ class Udacidata
     return destroyed_product
   end
 
-  def self.create_find_by_methods
-    @@attrs.each do |attr|
-      find_by_method = %Q{
-        def find_by_#{attr}(attr_value)
-         found = all.select { |product| product.#{attr} ==  attr_value}
-         return found.first
-        end
-        }
-      instance_eval(find_by_method)
-    end
-  end
-
   def self.where(opts = {})
     array_of_products = all.select { |product| product.brand == opts[:brand]} if opts[:brand]
     array_of_products = all.select { |product| product.name == opts[:name]} if opts[:name]
@@ -73,5 +61,5 @@ class Udacidata
     return self
   end
 
-  create_find_by_methods
+  create_finder_methods(:brand, :name)
 end
